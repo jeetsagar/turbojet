0b205b521ac9bd2f6f8ea4c70dc9e4ddd5049004
jeetsagar <jeetsagar@live.com>
2021-05-13 18:07:27 +0530

    working on dataset

--- src/explore.py
+++ src/explore.py
@@ -1,16 +1,14 @@
 #!python3
 
 import h5py
-
 import numpy as np
 
-filename = '../../data_set/N-CMAPSS_DS02-006.h5'
+def print_keys(filename):
+    with h5py.File(filename, 'r') as hdf:
+        keylist = list(hdf.keys())
+        varkeys = [i for i in keylist if i.endswith('var')]
 
-with h5py.File(filename, 'r') as hdf:
-    keylist = list(hdf.keys())
-    varkeys = [i for i in keylist if i.endswith('var')]
-
-    for k in varkeys:
-        vararr = np.array(hdf.get(k))
-        varlist = list(np.array(vararr, dtype='U20'))
-        print(k, varlist)
+        for k in varkeys:
+            vararr = np.array(hdf.get(k))
+            varlist = list(np.array(vararr, dtype='U20'))
+            print(k, varlist)
